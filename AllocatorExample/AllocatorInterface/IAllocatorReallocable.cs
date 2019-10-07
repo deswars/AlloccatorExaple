f965cc28e0f6e806a798afdf22fa6601abd0cffe
Nikolay Mishcheriakov <deswars2011@gmail.com>
2019-10-07 14:48:59 +0300

    add: allocator interface with relloc

---
+++ AllocatorExample/AllocatorInterface/IAllocatorReallocable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AllocatorInterface
+{
+    interface IAllocatorReallocable : IAllocator
+    {
+        public uint Relloc(uint address, uint newSize);
+    }
+}
