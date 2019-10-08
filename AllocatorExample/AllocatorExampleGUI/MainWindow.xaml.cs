b02da98f75e4ee7af5df3e65639a3139a196fa90
Nikolay Mishcheriakov <deswars2011@gmail.com>
2019-10-08 17:16:31 +0300

    add: Simple GUI for allocator test

--- AllocatorExample/AllocatorExampleGUI/MainWindow.xaml.cs
+++ AllocatorExample/AllocatorExampleGUI/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using AllocatorInterface;
+using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -23,6 +26,48 @@ namespace AllocatorExampleGUI
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog
+            {
+                Filter = "Assemblies (*.dll)|*.dll"
+            };
+            if (dialog.ShowDialog() == true)
+            {
+                try
+                {
+                    Assembly assembly = Assembly.LoadFile(dialog.FileName);
+                    var type = typeof(IAllocatorBuilder);
+                    var list = AppDomain.CurrentDomain.GetAssemblies().SelectMany(s => s.GetTypes()).Where(p => type.IsAssignableFrom(p) && p.IsClass);
+                    LstbxBuilders.Items.Clear();
+                    foreach (var builder in list)
+                    {
+                        LstbxBuilders.Items.Add(builder);
+                    }
+                }
+                catch
+                {
+                    MessageBox.Show("Cannot load assembly", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+
+        private void BtnRun_Click(object sender, RoutedEventArgs e)
+        {
+            TestWindow testWindow = new TestWindow
+            {
+                Builder = (Type)LstbxBuilders.SelectedItem,
+                Main = this
+            };
+            testWindow.Show();
+            Visibility = Visibility.Hidden;
+        }
+
+        private void LbBuilders_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            BtnRun.IsEnabled = true;
         }
     }
 }
---
+++ AllocatorExample/AllocatorExampleGUI/TestWindow.xaml.cs
@@ -0,0 +1,152 @@
+﻿using AllocatorInterface;
+using MemoryModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AllocatorExampleGUI
+{
+    /// <summary>
+    /// Interaction logic for TestWindow.xaml
+    /// </summary>
+    public partial class TestWindow : Window
+    {
+        public Window Main;
+        public Type Builder;
+
+        public TestWindow()
+        {
+            InitializeComponent();
+        }
+
+        private Dictionary<int, Color> _colors;
+        private IAllocatorBuilder _builder;
+        private IAllocator _allocator;
+        private IAllocatorAnalizer _analizer;
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            Main.Visibility = Visibility.Visible;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            _builder = (IAllocatorBuilder)Activator.CreateInstance(Builder);
+
+            List<Color> colorList = new List<Color>{ Colors.White, Colors.Green, Colors.Blue, Colors.Red, Colors.Orange };
+            _colors = new Dictionary<int, Color>();
+            var names = Enum.GetNames(typeof(MemoryAnalizerStatus)).ToArray();
+            var values = Enum.GetValues(typeof(MemoryAnalizerStatus)).Cast<int>().ToArray();
+            for (int i = 0; i < names.Length; i++)
+            {
+                _colors.Add(i, colorList[i]);
+                Grid colorGrid = new Grid();
+                ColumnDefinition firstColumn = new ColumnDefinition();
+                firstColumn.Width = new GridLength(100);
+                colorGrid.ColumnDefinitions.Add(firstColumn);
+                ColumnDefinition secondColumn = new ColumnDefinition();
+                secondColumn.Width = new GridLength(50);
+                colorGrid.ColumnDefinitions.Add(secondColumn);
+
+                Label colorLabel = new Label();
+                colorLabel.Content = names[i];
+                Grid.SetColumn(colorLabel, 0);
+                colorGrid.Children.Add(colorLabel);
+
+                Border colorRectangle = new Border();
+                colorRectangle.Width = 50;
+                colorRectangle.Background = new SolidColorBrush(colorList[i]);
+                Grid.SetColumn(colorRectangle, 1);
+                colorGrid.Children.Add(colorRectangle);
+
+                LstbxColors.Items.Add(colorGrid);
+            }
+        }
+
+        private void BtnMemory_Click(object sender, RoutedEventArgs e)
+        {
+            bool isValid = uint.TryParse(TbMemory.Text, out uint size);
+            if (isValid && (size > 100))
+            {
+                TbAlloc.IsEnabled = true;
+                BtnAlloc.IsEnabled = true;
+                LstbxAlloc.Items.Clear();
+                BtnFree.IsEnabled = false;
+
+                Memory memory = new Memory(size);
+                _builder.SetMemory(memory);
+                _allocator = _builder.Build();
+                _analizer = _builder.BuildAnalizer();
+                ShowMemoryStatus();
+            }
+        }
+
+        private void ShowMemoryStatus()
+        {
+            MemoryAnalizerStatus[] memory = _analizer.AnalizeMemory();
+            CvMemoryStatus.Children.Clear();
+            int memoryCellSize = 4;
+            int x = 0;
+            int y = 0;
+            foreach (var cell in memory)
+            {
+                Rectangle rect = new Rectangle();
+                rect.Fill = new SolidColorBrush(_colors[(int)cell]);
+                rect.Width = memoryCellSize;
+                rect.Height = memoryCellSize;
+                Canvas.SetLeft(rect, x);
+                Canvas.SetTop(rect, y);
+                CvMemoryStatus.Children.Add(rect);
+
+                x += memoryCellSize;
+                if (x >= CvMemoryStatus.ActualWidth)
+                {
+                    x = 0;
+                    y += memoryCellSize;
+                }
+            }
+        }
+
+        private void BtnAlloc_Click(object sender, RoutedEventArgs e)
+        {
+            bool isValid = uint.TryParse(TbAlloc.Text, out uint size);
+            if (isValid)
+            {
+                uint addr = _allocator.Alloc(size);
+                if (addr != _allocator.Null)
+                {
+                    LstbxAlloc.Items.Add(addr);
+                    LstbxAlloc.IsEnabled = true;
+                    ShowMemoryStatus();
+                }
+                else
+                {
+                    MessageBox.Show("Cannot allocate", "Error", MessageBoxButton.OK, MessageBoxImage.Warning);
+                }
+            }
+        }
+
+        private void LstbxAlloc_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            BtnFree.IsEnabled = true;
+        }
+
+        private void BtnFree_Click(object sender, RoutedEventArgs e)
+        {
+            uint address = (uint)LstbxAlloc.SelectedValue;
+            LstbxAlloc.Items.Remove(LstbxAlloc.SelectedItem);
+            _allocator.Free(address);
+            BtnFree.IsEnabled = false;
+            ShowMemoryStatus();
+        }
+    }
+}
